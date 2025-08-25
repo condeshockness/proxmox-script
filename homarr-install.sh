472fcf2c8f683cc94b3335725ffb4bbdc749ad00
condeshockness <ramonsilas.dev@gmail.com>
2025-08-25 14:53:09 -0300

    Update homarr-install.sh

--- homarr-install.sh
+++ homarr-install.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2021-2025 community-scripts ORG
-# Author: MickLesk (Canbiz)
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://github.com/homarr-labs/homarr
-
-source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
-color
-verb_ip6
-catch_errors
-setting_up_container
-network_check
-update_os
 
 msg_info "Installing Dependencies"
 $STD apt-get install -y \
