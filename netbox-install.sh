d8e7526658a7925840f6a00a6a42a6b778aee988
condeshockness <ramonsilas.dev@gmail.com>
2025-10-20 13:52:53 -0400

    ok

--- netbox-install.sh
+++ netbox-install.sh
@@ -34,13 +34,13 @@ msg_ok "Installed Dependencies"
 msg_info "Setting up PostgreSQL"
 DB_NAME=netbox
 DB_USER=netbox
-DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)
-$STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
+#DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)
+$STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '123456';"
 $STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMPLATE template0;"
 {
   echo "Netbox-Credentials"
   echo -e "Netbox Database User: \e[32m$DB_USER\e[0m"
-  echo -e "Netbox Database Password: \e[32m$DB_PASS\e[0m"
+  echo -e "Netbox Database Password: \e[32m123456\e[0m"
   echo -e "Netbox Database Name: \e[32m$DB_NAME\e[0m"
 } >>~/netbox.creds
 msg_ok "Set up PostgreSQL"
@@ -65,9 +65,10 @@ ESCAPED_SECRET_KEY=$(printf '%s\n' "$SECRET_KEY" | sed 's/[&/\]/\\&/g')
 sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ["*"]/' /opt/netbox/netbox/netbox/configuration.py
 sed -i "s|SECRET_KEY = ''|SECRET_KEY = '${ESCAPED_SECRET_KEY}'|" /opt/netbox/netbox/netbox/configuration.py
 sed -i "/DATABASE = {/,/}/s/'USER': '[^']*'/'USER': '$DB_USER'/" /opt/netbox/netbox/netbox/configuration.py
-sed -i "/DATABASE = {/,/}/s/'PASSWORD': '[^']*'/'PASSWORD': '$DB_PASS'/" /opt/netbox/netbox/netbox/configuration.py
+sed -i "/DATABASE = {/,/}/s/'PASSWORD': '[^']*'/'PASSWORD': '123456'/" /opt/netbox/netbox/netbox/configuration.py
 
 $STD /opt/netbox/upgrade.sh
+sudo PYTHON=/usr/bin/python3.10 /opt/netbox/upgrade.sh
 ln -s /opt/netbox/contrib/netbox-housekeeping.sh /etc/cron.daily/netbox-housekeeping
 
 mv /opt/netbox/contrib/apache.conf /etc/apache2/sites-available/netbox.conf
