d10ade2fa051f89738a8b7d18f126cab1bfa5187
condeshockness <ramonsilas.dev@gmail.com>
2025-11-11 13:48:39 -0400

    ok

--- influxdb.sh
+++ influxdb.sh
@@ -13,27 +13,28 @@ $STD apt-get install -y lsb-release
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up InfluxDB Repository"
-curl -fsSL "https://repos.influxdata.com/influxdata-archive_compat.key" | gpg --dearmor >/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main" >/etc/apt/sources.list.d/influxdata.list
-msg_ok "Set up InfluxDB Repository"
 
-read -r -p "${TAB3}Which version of InfluxDB to install? (1 or 2) " prompt
-if [[ $prompt == "2" ]]; then
-  INFLUX="2"
-else
-  INFLUX="1"
-fi
+
+
+# Add the InfluxData key to verify downloads and add the repository
+curl --silent --location -O https://repos.influxdata.com/influxdata-archive.key
+gpg --show-keys --with-fingerprint --with-colons ./influxdata-archive.key 2>&1 \
+| grep -q '^fpr:\+24C975CBA61A024EE1B631787C3D57159FC2F927:$' \
+&& cat influxdata-archive.key \
+| gpg --dearmor \
+| tee /etc/apt/keyrings/influxdata-archive.gpg > /dev/null \
+&& echo 'deb [signed-by=/etc/apt/keyrings/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' \
+| tee /etc/apt/sources.list.d/influxdata.list
+
+msg_ok "Set up InfluxDB Repository"
 
 msg_info "Installing InfluxDB"
 $STD apt-get update
-if [[ $INFLUX == "2" ]]; then
-  $STD apt-get install -y influxdb2
-else
-  $STD apt-get install -y influxdb
-  curl -fsSL "https://dl.influxdata.com/chronograf/releases/chronograf_1.10.7_amd64.deb" -o "/chronograf_1.10.7_amd64.deb"
-  $STD dpkg -i chronograf_1.10.7_amd64.deb
-fi
+
+$STD apt-get install -y influxdb2
+
 $STD systemctl enable --now influxdb
+
 msg_ok "Installed InfluxDB"
 
 read -r -p "${TAB3}Would you like to add Telegraf? <y/N> " prompt
